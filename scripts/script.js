b13ea73cdb26ff46c2b5d9ded52857efbad2777d
Dmitriy Lezhnin <Dima-leg@yandex.ru>
2020-09-20 06:57:25 +0300

    feat: like

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ const placeAddButton = document.querySelector('.profile__add-button');
 const profileEditCloseButton = popupProfileEdit.querySelector('.popup__close-button');
 const placeAddCloseButton = popupPlaceAdd.querySelector('.popup__close-button');
 const popupSaveButton = document.querySelector('.popup__save-button');
+const delButton = document.querySelector('.place-card__recycleButton');
 const nameProfile = document.querySelector('.profile__name');
 const jobProfile = document.querySelector('.profile__job');
 const nameInput = document.querySelector('.popup__input_name');
@@ -39,17 +40,27 @@ const initialCards = [
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
-function renderItem ({label, link}) {
+//Собираем карточку по частям и добавляем в массив
+function renderItem({ label, link }) {
     const htmlElement = cardTemplate.content.cloneNode(true);
     htmlElement.querySelector('.place-card__header').innerText = label;
     htmlElement.querySelector('.place-card__image').src = link;
+    //like
+    const like = htmlElement.querySelector('.place-card__like-button');
+    like.addEventListener('click', function (x) {
+        x.target.classList.toggle('place-card__like');
+    })
+
     listCards.appendChild(htmlElement);
 }
+//Выводим массив карточек на экран
 function render() {
     listCards.innerHTML = "";
     initialCards.forEach(renderItem);
 }
-function popupToggle(x) {x.classList.toggle('popup_opened')};
+//Функция открытия-закрытия попапа
+function popupToggle(x) { x.classList.toggle('popup_opened') };
+//Обработчик для редактирования профиля
 // Находим форму для Edit в DOM
 let formProfileEdit = popupProfileEdit.querySelector('.popup__form');
 //Функция для вставки данных в форму
@@ -59,33 +70,42 @@ const defaultUserData = () => {
 }
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function profileEditSubmitHandler (evt) {
-    evt.preventDefault();  
+function profileEditSubmitHandler(evt) {
+    evt.preventDefault();
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
     popupToggle(popupProfileEdit);
 }
+//Обработчик для добавления места
 // Находим форму для Add Mesto в DOM
 let formPlaceAdd = popupPlaceAdd.querySelector('.popup__form');
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function placeAddSubmitHandler (evt) {
+function placeAddSubmitHandler(evt) {
     evt.preventDefault();
+    //Добавим введенные значения в переменные карточки
     const addCardValues = {
         label: placeNameInput.value,
         link: placeImgInput.value
     }
     initialCards.unshift(addCardValues);
-    formPlaceAdd.reset();    
+    formPlaceAdd.reset();
     popupToggle(popupPlaceAdd);
     render();
-}  
+}
+//delete card Mesto
 
-render ();
+
+
+
+
+//listeners
 formProfileEdit.addEventListener('submit', profileEditSubmitHandler);
 formPlaceAdd.addEventListener('submit', placeAddSubmitHandler);
-profileEditButton.addEventListener('click', () => {defaultUserData()});
-profileEditButton.addEventListener('click', () => {popupToggle(popupProfileEdit)});
-placeAddButton.addEventListener('click', () => {popupToggle(popupPlaceAdd)});
-profileEditCloseButton.addEventListener('click', () => {popupToggle(popupProfileEdit)});
-placeAddCloseButton.addEventListener('click', () => {popupToggle(popupPlaceAdd)});
+profileEditButton.addEventListener('click', () => { defaultUserData() });
+profileEditButton.addEventListener('click', () => { popupToggle(popupProfileEdit) });
+placeAddButton.addEventListener('click', () => { popupToggle(popupPlaceAdd) });
+profileEditCloseButton.addEventListener('click', () => { popupToggle(popupProfileEdit) });
+placeAddCloseButton.addEventListener('click', () => { popupToggle(popupPlaceAdd) });
+
+render();
