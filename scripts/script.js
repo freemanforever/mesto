6898e6e33621a0bf2ff00dbc7f12119f8b75e395
Dmitriy Lezhnin <Dima-leg@yandex.ru>
2020-10-06 01:42:23 +0300

    fixes

--- scripts/script.js
+++ scripts/script.js
@@ -158,11 +158,4 @@ function render() {
 //отрисуем
 render();
 //завалидируем
-enableValidation({
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__save-button',
-    inactiveButtonClass: 'popup__button_disabled',
-    inputErrorClass: 'popup__input_type-error',
-    errorClass: 'popup__error_visible'
-});
+enableValidation(params);
